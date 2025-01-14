92900f1e4464c2cb43b9ed7045116c4fac26671d
unknown <srimandgl2004@gmail.com>
2025-01-14 11:32:20 +0530

    Added update and delete functionality for movies with instant UI refresh

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const routes = require('./routes'); 
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); 
 app.use(express.json());
 app.use(routes); 
 
